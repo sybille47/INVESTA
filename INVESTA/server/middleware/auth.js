398b5cc5fdc29b5ec7cef7448956c39a4629417a
sybille47 <sybillezehringer@gmail.com>
2025-11-11 23:51:25 +0100

    Update Auth0 middleware to use environment variables

--- INVESTA/server/middleware/auth.js
+++ INVESTA/server/middleware/auth.js
@@ -2,9 +2,9 @@ const { auth } = require('express-oauth2-jwt-bearer');
 
 // Auth0 JWT verification middleware
 const checkJwt = auth({
-  audience: 'https://investa-api', // From Auth0 API settings
-  issuerBaseURL: 'https://dev-3j603j6lo2akvuu6.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: process.env.AUTH0_TOKEN_SIGNING_ALG || 'RS256'
 });
 
 module.exports = { checkJwt };
